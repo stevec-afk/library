2d0db4ba510444da189d823d009a456337fab8a2
stevec-afk <evets25@gmail.com>
2025-12-09 15:32:49 -0400

    CSS redesign

---
+++ library.js
@@ -0,0 +1,20 @@
+let library = [
+    {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkein",
+        pages: 1216,
+        read: true
+    }
+]
+
+class Book {
+    constructor(author, title, pages, read){
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
+    }
+
+
+
+}
